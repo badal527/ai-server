302695afb8abad615af1940d5ce2a0b2faaa75ef
badal527 <badaltomar527@gmail.com>
2026-03-27 09:59:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("AI Server Running 🚀");
+});
 
 // TEST ROUTE
 app.get("/", (req, res) => {
